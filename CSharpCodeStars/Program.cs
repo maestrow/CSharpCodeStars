92f69422de5ae3d6c99850275bbbcac9732c57dd
maestrow <andrew.shmelev@gmail.com>
2015-01-05 21:14:35 +0300

    Решена задача № 10

--- CSharpCodeStars/Program.cs
+++ CSharpCodeStars/Program.cs
@@ -10,7 +10,7 @@ namespace CSharpCodeStars
     {
         static void Main(string[] args)
         {
-            IStep step = new Step9();
+            IStep step = new Step10();
             step.Do(Console.Out);
             Console.WriteLine("ok.");
             Console.ReadKey();
